1e2a7bc1f0f7f459fb573f798b47d965f717400e
Jacob Hands <jacob@gogit.io>
2023-02-20 23:27:39 +0000

    Started on multiple embeds

---
+++ src/utils.ts
@@ -0,0 +1,25 @@
+import { ThrottledQueue } from '@jahands/msc-utils'
+import { Env } from './types'
+
+// Inject rate-limiting (this is not a great way to do this, but it works..)
+const defaultQueue = new ThrottledQueue({ concurrency: 1, interval: 500, limit: 1 });
+export async function getDiscordWebhook(from: string, env: Env): Promise<string> {
+	await defaultQueue.add(async () => { })
+	let hook = env.DISCORDHOOK
+	if (from === 'noreply@github.com') {
+		hook = env.GITHUBHOOK
+	} else if (from === 'notifications@disqus.net') {
+		hook = env.DISQUSHOOK
+	} else if (from.match(/^postmaster@mail[a-z0-9-]*\.google\.com$/)) {
+		hook = env.GERRITHOOK
+	}
+	return hook
+}
+
+export function getAuthHeader(env: Env): { Authorization: string } {
+	return { Authorization: `Bot ${env.BOTTOKEN}` }
+}
+
+export function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
