abda7d21f3150dcc25f52155f480e70477edf515
Jacob Hands <jacob@gogit.io>
2023-02-20 17:54:11 -0600

    Switch to multiple embeds per hook

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
+import { sendDiscordEmbeds } from './discord';
 import { EmbedQueueData, Env } from './types'
+import { getDiscordWebhook } from './utils';
 
 export default {
 	async queue(batch: MessageBatch<EmbedQueueData>, env: Env, ctx: ExecutionContext) {
-		// Extract the body from each message.
-		// Metadata is also available, such as a message id and timestamp.
-		for (const message of batch.messages) {
-			await sendDiscordEmbed(message.body, env, ctx)
+		// Different webhooks for different senders, so we need to group by webhook
+		const messages = batch.messages.map((m) => m.body)
+		const messagesByWebhook = {} as Record<string, EmbedQueueData[]>
+		for (const m of messages) {
+			const webhook = await getDiscordWebhook(m.from, env)
+			if (!messagesByWebhook[webhook]) {
+				messagesByWebhook[webhook] = []
+			}
+			messagesByWebhook[webhook].push(m)
+		}
+	
+		for (const webhook of Object.keys(messagesByWebhook)) {
+			await sendDiscordEmbeds(messagesByWebhook[webhook], webhook, env, ctx)
 		}
 	},
 };
--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,12 @@ import { ThrottledQueue } from '@jahands/msc-utils'
 import { Env } from './types'
 
 // Inject rate-limiting (this is not a great way to do this, but it works..)
-const defaultQueue = new ThrottledQueue({ concurrency: 1, interval: 500, limit: 1 });
+const defaultQueue = new ThrottledQueue({ concurrency: 1, interval: 1000, limit: 1 });
+
 export async function getDiscordWebhook(from: string, env: Env): Promise<string> {
 	await defaultQueue.add(async () => { })
 	let hook = env.DISCORDHOOK
-	if (from === 'noreply@github.com') {
+	if (from === 'noreply@github.com' || from.endsWith('@sgmail.github.com')) {
 		hook = env.GITHUBHOOK
 	} else if (from === 'notifications@disqus.net') {
 		hook = env.DISQUSHOOK
