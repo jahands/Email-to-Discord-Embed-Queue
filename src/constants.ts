b869cf5a6d16c1f2ad408da2617afdc8a8800764
Jacob Hands <jacob@gogit.io>
2023-02-20 18:43:05 -0600

    Use max size

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const DISCORD_EMBED_LIMIT = 4096 - 50 // Just to be safe
+export const DISCORD_EMBED_LIMIT = 4096
 export const DISCORD_TOTAL_LIMIT = 6000 // Across all embeds
 // This may be higher if your server is boosted to level 2, it should be 50MB. If your server is boosted to level 3, it should be 100MB.
 export const DISCORD_FILE_LIMIT = 8000000
