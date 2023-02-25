0d02846983e5b9963a18b5031d9b998cf71c4863
Jacob Hands <jacob@gogit.io>
2023-02-25 15:18:34 -0600

    Add chromium-reviews to gerrit

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,27 @@
 import { Env } from './types'
 
-export async function getDiscordWebhook(from: string, env: Env): Promise<{name: string, hook: string}> {
+export async function getDiscordWebhook(from: string, env: Env): Promise<{ name: string, hook: string }> {
 	if (from === 'noreply@github.com'
 		|| from === 'notifications@github.com'
 		|| from.endsWith('@sgmail.github.com')
 	) {
-		return {hook: env.GITHUBHOOK, name: 'github'}
+		return { hook: env.GITHUBHOOK, name: 'github' }
 	} else if (from === 'notifications@disqus.net') {
-		return {hook: env.DISQUSHOOK, name: 'disqus'}
-	} else if (from.match(/^postmaster@mail[a-z0-9-]*\.google\.com$/)) {
-		return {hook: env.GERRITHOOK, name: 'gerrit'}
+		return { hook: env.DISQUSHOOK, name: 'disqus' }
+	} else if (isGerrit(from)) {
+		return { hook: env.GERRITHOOK, name: 'gerrit' }
 	} else if (from.endsWith('@alerts.bounces.google.com')) {
 		return { hook: env.GOOGLEALERTSHOOK, name: 'google_alerts' }
 	}
-	return {hook: env.DISCORDHOOK, name: 'default'}
+	return { hook: env.DISCORDHOOK, name: 'default' }
+}
+
+function isGerrit(from: string) {
+	const fromRe = [
+		/^postmaster@mail[a-z0-9-]*\.google\.com$/, // most gerrit
+		/^chromium-reviews\+\w+@chromium\.org$/, // chromium gerrit
+	]
+	return fromRe.some(re => re.test(from))
 }
 
 export function getAuthHeader(env: Env): { Authorization: string } {
