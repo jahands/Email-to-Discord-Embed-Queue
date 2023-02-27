0aeb8edf3c5d49e474d176bd692051a2645d4988
Jacob Hands <jacob@gogit.io>
2023-02-26 22:52:10 -0600

    Remove log

--- src/discord.ts
+++ src/discord.ts
@@ -33,7 +33,6 @@ export async function sendDiscordEmbeds(messages: EmbedQueueData[],
 
 		// Recording some stats here since we're parsing anyway
 		if (message.from.match(/messages@\w+\.govdelivery\.com/)) {
-			console.log('Recording GovDelivery stats')
 			try {
 				const govDeliveryID = getGovDeliveryID(text)
 				govDeliveryStats.set(govDeliveryID, (govDeliveryStats.get(govDeliveryID) || 0) + 1)
