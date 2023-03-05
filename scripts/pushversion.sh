954a5557c66c2e653efde34f48099d191bf7f665
Jacob Hands <jacob@gogit.io>
2023-03-04 20:38:19 -0600

    Update scripts

---
+++ scripts/pushversion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Push to remote only if there is a single commit ahead of the remote
+# (i.e. the commit that was just made by the version script)
+branch=$(git branch --show-current)
+if [ $(git rev-list --count origin/$branch...$branch) -eq 1 ]; then
+  git push $branch origin/$branch
+fi
